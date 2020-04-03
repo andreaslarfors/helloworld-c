54229d68825a111895bda42fa191197a173dabe6
Andreas Lärfors <andreas@larfors.com>
2020-04-03 13:42:24 +0200

    Cleanup and drone

--- hello.c
+++ hello.c
@@ -2,18 +2,6 @@
 int main(void)
 {
    printf ("Hello world!\n");
-  
-   int a, b;
-   if (a=b){
-   }
-   if (a=b){
-}
-   if (a=b){
-}
-   int* i = NULL;
-   *i = 5;
-
-   return 0;
 }
 
 //test comment
